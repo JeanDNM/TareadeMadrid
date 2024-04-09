acaabfa394aa6166b462c94a693d8dda82902b34
jean <jeanneyra06@gmail.com>
2024-04-09 09:22:56 -0700

    Initial Commit

---
+++ botones/botonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  botonesApp.swift
+//  botones
+//
+//  Created by Jeanpierre on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct botonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
